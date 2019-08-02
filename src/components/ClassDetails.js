95ad73b7463b37a005e67510cb97db19ae3416d5
Sam Heyman <sam.heyman@gmail.com>
2019-08-02 17:05:26 +0200

    Fix page load issue on ClassDetails: move to top of page

--- src/components/ClassDetails.js
+++ src/components/ClassDetails.js
@@ -1,46 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import DesktopHeader from './Header/DesktopHeader';
 import DesktopNavigation from './DesktopNavigation';
 import MobileBackHeader from './Header/MobileBackHeader';
 
-const ClassDetails = (props) => {
-    const divStyle = {
-        backgroundImage: 'url(../' + props.selectedClass.image + ')',
-    };
+class ClassDetails extends Component {
 
-    return(
-        <React.Fragment>
-            <MobileBackHeader link="/classes" />
-            
-            <DesktopHeader/>
-            <div className="row">
-                <div className="container">
-                    <DesktopNavigation />
-                    <div className="col s12 m9 l9">
-                        <div className="class-image" style={divStyle}>
-                        </div>
-                        <div className="class-details">
-                            <div>
-                                <span className="card-title">{props.selectedClass.className}</span>
-                                <span className="card-subtitle">{props.selectedClass.companyName}</span>
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        window.scrollTo(0,0);
+    }
+
+    render() {
+
+        const divStyle = {
+            backgroundImage: 'url(../' + this.props.selectedClass.image + ')',
+        };
+
+        return(
+            <React.Fragment>
+                <MobileBackHeader link="/classes" />
+                
+                <DesktopHeader/>
+                <div className="row">
+                    <div className="container">
+                        <DesktopNavigation />
+                        <div className="col s12 m9 l9">
+                            <div className="class-image" style={divStyle}>
                             </div>
-                            <div className="card-footer">
-                                <div className="datetime">
-                                    <span>
-                                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(props.selectedClass.date)))}
-                                    {/* {classEntry.date} */}
-                                    </span>
+                            <div className="class-details">
+                                <div>
+                                    <span className="card-title">{this.props.selectedClass.className}</span>
+                                    <span className="card-subtitle">{this.props.selectedClass.companyName}</span>
                                 </div>
-                                <div className="price">
-                                    <span>{props.selectedClass.price}€</span>
+                                <div className="card-footer">
+                                    <div className="datetime">
+                                        <span>
+                                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(this.props.selectedClass.date)))}
+                                        {/* {classEntry.date} */}
+                                        </span>
+                                    </div>
+                                    <div className="price">
+                                        <span>{this.props.selectedClass.price}€</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </React.Fragment>
-    );
+            </React.Fragment>
+        );
+    }
 }
 
 export default ClassDetails;
