df5529d42f20b7cbdf75929397c5f87d3b6b244d
Sam Heyman <sam.heyman@gmail.com>
2019-08-18 14:49:29 +0200

    Add class tags with CSS, restyle class card layout

--- src/components/ClassCard.js
+++ src/components/ClassCard.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+function RenderTags({tags}) {
+    const output = tags.map((tag) => {
+        return (
+            <span className={`tag tag-${tag}`}>{tag}</span>
+        );
+    });
+
+    return output;
+}
+
 const ClassCard = (props) => {
     const divStyle = {
         backgroundImage: 'url(images/' + props.classEntry.image + ')',
@@ -13,25 +23,72 @@ const ClassCard = (props) => {
                     </div>
                     <div className="card-content">
                         <div className="card-header">
-                            <span className="card-title">{props.classEntry.className}</span>
-                            <span className="card-subtitle">{props.classEntry.companyName}</span>
-                            <span className="card-text">{props.classEntry.address}</span>
+                            <div className="company">
+                                <img className="logo" src={`../images/logos/${props.classEntry.companyLogo}`}></img>
+                                <div className="company-details">
+                                    <div className="company-name">
+                                        {props.classEntry.companyName}
+                                    </div>
+                                    <div className="company-address truncate">
+                                        {props.classEntry.district}
+                                    </div>
+                                </div>
+                            </div>   
+                            <span className="card-title truncate">{props.classEntry.className}</span>
+                            <div className="class-tags tags">
+                                <RenderTags tags={props.classEntry.tags} />
+
+                                {/* <div className="class-date ">
+                                    <i className="material-icons date_range">date_range</i>
+                                    <span>{new Intl.DateTimeFormat('en-US', { weekday: 'short', day: '2-digit', month: 'short' }).format(new Date(Date.parse(props.classEntry.date)))}</span>
+                                </div>
+                                <div className="class-time ">
+                                    <i className="material-icons access_time">access_time</i>
+                                    <span>{props.classEntry.time}</span>
+                                </div> */}
+                                {/* <div className="class-duration truncate">
+                                    <i className="material-icons timelapse">timelapse</i>
+                                    <span>{props.classEntry.duration}</span>
+                                </div> */}
+                            </div>
+                                {/* <span className="card-text">
+                                    {props.classEntry.time}
+                                </span> */}
+                            {/* <span className="card-text">{props.classEntry.address}</span> */}
                         </div>
                         <div className="card-footer">
                             <div className="left-div">
-                                <span>
-                                {new Intl.DateTimeFormat('en-US', { weekday: 'short', day: '2-digit', month: 'short' }).format(new Date(Date.parse(props.classEntry.date)))}
-                                <br />
-                                {props.classEntry.time}
+                                <div className="class-duration truncate">
+                                    <i className="material-icons timelapse">timelapse</i>
+                                    <span> {props.classEntry.duration}</span>
+                                </div>
+                                {/* <span>{props.classEntry.category}</span> */}
+                                {/* <img className="language-flag" src={`images/flags/${props.classEntry.language}.png`} alt="Class in English" /> */}
+                                {/* <span className="card-text">
+                                    {new Intl.DateTimeFormat('en-US', { weekday: 'short', day: '2-digit', month: 'short' }).format(new Date(Date.parse(props.classEntry.date)))}
                                 </span>
+                                <span className="card-text">
+                                    {props.classEntry.time}
+                                </span> */}
+                                
+                                
+
                             </div>
-                            {/* <div className="center-div">
-                                <i className="material-icons">access_time</i>
-                                <br />
-                                <span>{props.classEntry.duration}</span>
-                            </div> */}
+                            <div className="center-div">
+                                    {/* <span>{props.classEntry.duration}</span> */}
+                                {/* <span className="class-price">{props.classEntry.price}€</span> */}
+                            </div>
                             <div className="right-div">
-                                <span>{props.classEntry.price}€</span>
+                                <div className="class-duration">
+                                    {/* <div className="duration-icon-div border-debug">
+                                        <i className="material-icons duration-icon">iav_timer</i>
+                                    </div> */}
+                                    <div className="duration">
+                                        {/* {props.classEntry.duration} */}
+                                        {props.classEntry.price}€
+
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         {/* <div className="card-action"></div> */}
