c3bcd4bed88a0ace6177d06f826d6ccc764696f7
Sam Heyman <sam.heyman@gmail.com>
2019-10-05 19:45:25 +0200

    Make sidebar icon turn to close button

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -31,6 +31,9 @@ export default function Navbar(props) {
     });
 
     const toggleDrawer = (side, open) => event => {
+      var element = document.getElementById("nav-icon3");
+      (element.className==="open") ? element.classList.remove("open") : element.classList.add("open");
+              
       if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
         return;
       }
@@ -112,10 +115,18 @@ export default function Navbar(props) {
               </ul>
             </Box>
             <Box display={{ xs: 'block', sm: 'block', md: 'none', lg: 'none', xl: 'none' }}>
-              <IconButton 
+            <div id="nav-icon3" 
+              aria-label="menu"
+              onClick = {toggleDrawer('top', true)}>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+              {/* <IconButton 
                 onClick={toggleDrawer('top', true)} className={`${classes.menuButton} menu-icon`} color="inherit" aria-label="menu">
                 <MenuIcon />
-              </IconButton>
+              </IconButton> */}
             </Box>
           </Toolbar>
         </AppBar>
